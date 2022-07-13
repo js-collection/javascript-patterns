3383ce6c76957c90747e60570122901ace4316a1
Bertz <92259138+bertz-tech@users.noreply.github.com>
2022-07-13 19:15:48 +0200

    Create class-classic-pattern.js

---
+++ class-classic-pattern.js
@@ -0,0 +1,27 @@
+class MYCLASSNAME
+{
+
+	constructor()
+	{ 
+		this.myfunctionone();
+		this.myfunctiontwo();
+	}
+
+	myfunctionone()
+	{
+		this.mydata = String(window.location);
+	}
+
+	myfunctiontwo()
+	{
+		this.mydata = String(window.location);
+	}
+
+	myfree()
+	{
+		return mydata = String(window.location);
+	}
+
+
+}
+const myclassname = new MYCLASSNAME();
